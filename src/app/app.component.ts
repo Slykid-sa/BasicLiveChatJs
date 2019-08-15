5c868c75b888f5592b7720c80791d50c08e1fc29
Leoshan Reddi <leoshanreddi@Leoshans-MacBook-Pro.local>
2019-08-15 09:00:19 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp4';
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public af: AngularFire) {
+    this.items = af.database.lost('/messages', {
+      query: {
+        limiToLast: 5
+      }
+    });
+
+    this.af.auth.subscribe(auth => {
+      if(auth) {
+        this.name = auth;
+      }
+    });
+  }
+
+  login() {  //this is the method to call up when logging in
+    this.af.auth.login({
+      provder: AuthProviders.Facebook,
+      method: AuthMethods.Popup  //redirect can also be used
+    })
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
+
+  chatSend(theirMessage: string) {
+    this.items.push({ message: theirMessage, name: this.name.facebook.displayName });
+    this.msgVal = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC1y-m19UUO1vA7d3O-6llS4SFmuIQdFbQ",
+  authDomain: "angularchatapp-231c9.firebaseapp.com",
+  databaseURL: "https://angularchatapp-231c9.firebaseio.com",
+  projectId: "angularchatapp-231c9",
+  storageBucket: "",
+  messagingSenderId: "958288557915",
+  appId: "1:958288557915:web:aac71c1933bd07fc"
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +21,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+   
